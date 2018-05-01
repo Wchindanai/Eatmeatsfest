a7e54d43b939c245d4d6d87d3d8b1699fc148f22
Chindanai Wasanchin <chindanai@entercorp.net>
2018-05-01 18:26:02 +0700

    comment call api

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -31,9 +31,9 @@ class Home extends React.Component {
       js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=122099135013153&autoLogAppEvents=1';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
-    const url = `http://eatmeatsfest.azurewebsites.net/API/Get_Detail?Language=TH`
-    const data = await axios.get(url)
-    console.log(data)
+    // const url = `http://eatmeatsfest.azurewebsites.net/API/Get_Detail?Language=TH`
+    // const data = await axios.get(url)
+    // console.log(data)
     const { FOOD } = json
     this.setState({
       foodList: FOOD,
