a2b165b1b22dd651f7a2936d87d3cabe4f37d40c
Chindanai Wasanchin <chindanai@entercorp.net>
2018-05-15 02:53:39 +0700

    set background

--- src/containers/ContactUs.jsx
+++ src/containers/ContactUs.jsx
@@ -5,14 +5,11 @@ const ContactSection = styled.section`
   p {
     font-family: 'Cordia New' ;
   }
-  background-image: url("/img/contact-us.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 700px;
+  background-color: #B5322F !important;
 `
 
 const ContactUsDetail = styled.div`
-  margin-top: 150px; 
+  margin-top: 3em;
   p {
     color: white;
   }
@@ -20,19 +17,19 @@ const ContactUsDetail = styled.div`
     color: white;
     text-decoration: underline;
   }
-  background-color: black;
-  padding: 10%;
 `
 const Contact = () => (
   <Fragment>
-    <ContactSection className={'section'}>
+    <ContactSection className={'section contact-banner'}>
       <div className="container">
         <div className={'columns'}>
           <ContactUsDetail className={'column has-text-centered'}>
-            <p className={'title is-3 is-white'}>โทร : 02-318-4388</p>
-            <p className={'title is-3'}>E-mail : <a href={'mailto:info@eatmeatsfest.com'}>info@eatmeatsfest.com</a></p>
-            <p className={'title is-3'}>Facebook : <a href="https://www.facebook.com/EatMeatsFest">@EatMeatsFest</a>
-            </p>
+            <div>
+              <p className={'title is-3 is-white'}>โทร : 02-318-4388</p>
+              <p className={'title is-3'}>E-mail : <a href={'mailto:info@eatmeatsfest.com'}>info@eatmeatsfest.com</a></p>
+              <p className={'title is-3'}>Facebook : <a href="https://www.facebook.com/EatMeatsFest">@EatMeatsFest</a>
+              </p>
+            </div>
           </ContactUsDetail>
         </div>
       </div>
