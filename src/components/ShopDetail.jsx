3c72940e3f04e55b8fd26a9c7c6aefd10a0442a0
Chindanai Wasanchin <chindanai@entercorp.net>
2018-06-01 22:25:36 +0700

    unlock limit print only 3

--- src/components/ShopDetail.jsx
+++ src/components/ShopDetail.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
+
 const Description = styled.p`
   line-height: 1.5 !important;
 `
 
-const ShopDetail = ({ shopDetail }) => (
+const ShopDetail = ({shopDetail}) => (
   <section className={'section'}>
     <div className="container">
-      <p className={'title is-2 has-text-centered'}> {shopDetail.SHOP_NAME} </p><br/>
+      <p className={'title is-2 has-text-centered'}> {shopDetail.SHOP_NAME} </p>
+      <br/>
       <div className={'columns'}>
         <div className={'column is-4'}>
           <figure className="image is-256x256">
-            <img src={shopDetail.LOGO_PATH} />
+            <img src={shopDetail.LOGO_PATH}/>
           </figure>
         </div>
         <div className={'column'}>
-          <Description className={'subtitle is-4' } dangerouslySetInnerHTML={{ __html: shopDetail.SHOP_DESCRIPTION }}/>
-          <br />
+          <Description className={'subtitle is-4'}
+                       dangerouslySetInnerHTML={{__html: shopDetail.SHOP_DESCRIPTION}}/>
+          <br/>
           {
             shopDetail.SHOP_IMAGE.map((img, index) => {
-              if (index < 3) {
-                return (
-                  <Image className={'image is-256'}>
-                    <img src={img.IMAGE_PATH} />
-                  </Image>
-                )
-              }
+              return (
+                <Image className={'image is-256'}>
+                  <img src={img.IMAGE_PATH}/>
+                </Image>
+              )
             })
           }
         </div>
       </div>
     </div>
   </section>
-);
+)
 
 const Image = styled.figure`
   display: inline-block !important;
   margin-right: 10px;
-`;
+`
 
-export default ShopDetail;
+export default ShopDetail
